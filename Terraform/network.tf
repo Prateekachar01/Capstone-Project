d96369ed9c99bb2a2aef3a43538872f890488bc3
Ubuntu <ubuntu@ip-172-31-88-241.ec2.internal>
2023-09-27 16:26:15 +0000

    changes

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -7,3 +7,31 @@ resource "aws_vpc" "vpc" {
     Name = "VPC-071" #VPC name
   }
 }
+
+#Creating the IGW
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "IGW-071" # Internet Gateway name
+  }
+}
+
+# Find available AZs
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+#  Creating 2 Public Subnets in each AZ
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.10.${1 + count.index}.0/24"  # CIDR calculation
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+tags = {
+    Name = "PublicSubnet-071-${count.index + 1}"
+     "kubernetes.io/cluster/my-eks-201" = "shared"
+    "kubernetes.io/roles/elb"        = "1"
+}
+}
+
